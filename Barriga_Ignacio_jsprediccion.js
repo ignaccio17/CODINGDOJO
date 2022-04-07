120cf8b7f1e65d3447ac291947c80d6a5b878c17
nachito0 <ignacio.barriga.munoz@alumnos.sip.cl>
2022-04-07 11:32:10 -0400

    Coding Dojo (Predecir funciones)

---
+++ Barriga_Ignacio_jsprediccion.js
@@ -0,0 +1,31 @@
+Prediccion 1//¿Qué indicará console.log cuando se llame a esta función?
+
+function myBirthYearFunc(){
+    console.log("Nací en " + 1980);
+}
+//console.log indica que naci en 1980
+
+----------------------------------------------------------
+Prediccion 2//¿qué indicará console.log?
+
+function myBirthYearFunc(EntradaAñoNacimiento){
+    console.log("Nací en " + EntradaAñoNacimiento);
+}
+//console.log indicara el año de nacimiento "EntradaAñoNacimiento" 
+//que es variable a 1980 
+
+----------------------------------------------------------
+Prediccion 3//Intentémoslo otra vez. Si var num1 = 10 y var num2 = 20, 
+//¿qué indicaría console.log?
+
+function add(num1, num2){
+    console.log("¡Sumando números!");
+    console.log("num1 is: " + num1);
+    console.log("num2 is: " + num2);
+    var sum = num1 + num2;
+    console.log(sum);
+}
+
+//console.log indica que num 1 = a 10 y
+//num2 = a 20 por lo tanto la suma de estos
+//dos daria como resultado 30
