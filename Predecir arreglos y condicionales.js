510c9ccb4fe171d9d4bfb43af3182d38691e2dda
nachito0 <ignacio.barriga.munoz@alumnos.sip.cl>
2022-04-12 10:38:17 -0400

    Predecir arreglos y condicionales

---
+++ Predecir arreglos y condicionales.js
@@ -0,0 +1,42 @@
+/*
+Fragmento de código 1
+//
+
+*/
+function muestraInformaciónContacto() {
+    var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo) 
+}
+/*
+VARIABLES                     DATOS
+auntContactInfo      =        ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345]
+
+
+----------------------------------------------------------------------------------------------------
+Fragmento de código 2
+*/
+function muestraListaDeCompras() {
+    var produce = ["manzanas", "naranjas"];
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croqueta de papa");
+    console.log(frozen);
+}
+/*
+VARIABLES                     DATOS
+produce          =            ["manzanas", "naranjas"]
+frozen           =            ["brócoli", "helado"]
+
+
+----------------------------------------------------------------------------------------------------
+Fragmento de código 3
+//
+*/
+
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+/*
+VARIABLES                     DATOS
+movieLibrary       =          ["Bambi", "E.T.", "Toy Story"];
+*/
