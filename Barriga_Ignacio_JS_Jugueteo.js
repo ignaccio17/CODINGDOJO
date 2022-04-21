73ab7e96f6df5126bd416180b3bccea4bd45e0a2
nachito0 <ignacio.barriga.munoz@alumnos.sip.cl>
2022-04-21 08:47:44 -0400

    Desafío de código opcional: JavaScript

--- Barriga_Ignacio_JS_Jugueteo.js
+++ Barriga_Ignacio_JS_Jugueteo.js
@@ -1,11 +1,11 @@
 
 
 function muestraSiElNinoPuedeSubirALaMontañaRusa (EntradaAlturaEdad){
-    var alturaNiño = 60;
+    // EntradaAlturaEdad = 60;
 
-    if (alturaNiño > 52 )
+    if (EntradaAlturaEdad > 52 )
     console.log('¡Súbete, chico!');
-    else (alturaNiño < 52)
+    else (EntradaAlturaEdad < 52)
     console.log('Lo siento, chico. Tal vez el próximo año');
 
 }
